a59640d88d6a483d14aa80b7179f47f034e1ee1b
Sebastian Serrano <serranosebastian23@gmail.com>
2023-02-07 15:39:55 -0800

    added a model class with one sttribute so far, class name

--- core/models.py
+++ core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class ClassTopic(models.Model):
+    class_name = models.CharField(max_length = 70)
+
+    def __str__(self):
+        return self.class_name
