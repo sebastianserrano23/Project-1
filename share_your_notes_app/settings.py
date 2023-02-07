9f37edcd685a8952c9f01e12f9d9a5275e0a0f76
Sebastian Serrano <serranosebastian23@gmail.com>
2023-02-07 15:42:26 -0800

    added core to my list of installed apps in settings.py, also migrated the first model

--- share_your_notes_app/settings.py
+++ share_your_notes_app/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # my apps 
+    "core",
+    # default apps
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
