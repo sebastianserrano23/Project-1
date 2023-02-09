c27858a47b5beb69a51f406f52b9ff0994b60c9d
Sebastian Serrano <serranosebastian23@gmail.com>
2023-02-09 00:45:17 -0800

    added a descriptions model so there could be a description of what the class is about

--- core/models.py
+++ core/models.py
@@ -1,8 +1,18 @@
 from django.db import models
 
 # Create your models here.
-class ClassTopic(models.Model):
-    class_name = models.CharField(max_length = 70)
+class ClassName(models.Model):
+    name = models.CharField(max_length = 70)
 
     def __str__(self):
-        return self.class_name
+        return self.name
+
+class Description(models.Model):
+    topic = models.ForeignKey(ClassName, on_delete = models.CASCADE)
+    text = models.TextField()
+
+    class Meta:
+        verbose_name_plural = 'Descriptions'
+    
+    def __str__(self):
+        return f"{self.text[:50]}..."
