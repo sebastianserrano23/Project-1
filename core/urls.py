729cf0642d0aa0234da6b7d41f64bdaa8adaec94
Sebastian Serrano <serranosebastian23@gmail.com>
2023-02-09 09:43:16 -0800

    created url path tom views 'index'

--- core/urls.py
+++ core/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name=index), # when we leave it blank like '', this means we are setting up our own url
+    path('', views.index, name='index'), # when we leave it blank like '', this means we are setting up our own url
 ]
