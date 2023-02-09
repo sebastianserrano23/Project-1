4d51e0c4bbb6a1433d2ed5fae7373e03c02420da
Sebastian Serrano <serranosebastian23@gmail.com>
2023-02-09 13:27:39 -0800

    registeirng my models into admin file

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,8 @@
+from msilib.schema import Class
 from django.contrib import admin
+from .models import ClassName, Description
 
 # Register your models here.
+models = [ClassName, Description]
+for model in models:
+    admin.site.register(model)
