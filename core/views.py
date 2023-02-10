bc691935f40ecdd031e5442e08e0280c12f909d5
Sebastian Serrano <serranosebastian23@gmail.com>
2023-02-09 16:41:17 -0800

    index function returns vews.html now

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
+from unicodedata import name
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Welcome to Peer Source</h1>')
+    return render(request, 'index.veiws', name='index')
