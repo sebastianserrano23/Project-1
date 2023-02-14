64339114712fe0acd7a49b4cd4c6ca5ecb4853ef
Sebastian Serrano <serranosebastian23@gmail.com>
2023-02-14 15:00:15 -0800

    i dont know, something

--- core/models.py
+++ core/models.py
@@ -18,7 +18,3 @@ class Description(models.Model):
     
     def __str__(self):
         return f"{self.text[:50]}..."
-
-class Image(models.Model):
-    image = models.ImageField(upload_to='images/')
-    created_at = models.DateTimeField(auto_now_add=True)
