4321e9b42af10f6d4f3f4d1cee4bdc7afa1ddae7
Sebastian Serrano <serranosebastian23@gmail.com>
2023-02-16 08:57:10 -0800

    feb 16, 2023.

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import ClassName, Description, Image
+from .models import ClassName, Description
 
 # Register your models here.
-models = [ClassName, Description, Image]
+models = [ClassName, Description]
 for model in models:
     admin.site.register(model)
--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = 'core'
 urlpatterns = [
-    path('', views.index, name='index'), # when we leave it blank like '', this means we are setting up our own url
+    # home page
+    path('', views.index, name = 'index')
 ]
