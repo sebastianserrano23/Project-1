36c1aa12fdf822ff34e6df353e0d5ecfe47a0a17
Sebastian Serrano <serranosebastian23@gmail.com>
2023-02-22 18:44:03 -0800

    added a nother class/model, UsserAccount, pretty much a bunch of basic attributes like email, username, password, etc, and some functions

--- UserAccounts/models.py
+++ UserAccounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-class UserManager(AbstractBaseUser):
+class UserManager(BaseUserManager):
     def create_user(self, username, email, password = None):
         if not email:
             raise ValueError("Not a valid email address")
@@ -24,4 +24,30 @@ class UserManager(AbstractBaseUser):
         user.is_admin = True
         user.save(using=self._db)
         return user
+
+class UserAccount(AbstractBaseUser):
+    email = models.EmailField(verbose_length="email address", max_length=200)
+    username = models.CharField(max_length=30, unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(null=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+    
+    @property
+    def is_staff(self):
+        return self.is_admin
 
