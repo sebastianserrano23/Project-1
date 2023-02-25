3e2841e7231f65d953d37b44642ab86ac03b7e77
Sebastian Serrano <serranosebastian23@gmail.com>
2023-02-24 22:34:58 -0800

    added an extra attribute to notes class

--- core/models.py
+++ core/models.py
@@ -1,12 +1,13 @@
 from django.core.validators import FileExtensionValidator
-from distutils.command.upload import upload
 from django.db import models
+from UserAccounts.models import UserAccount
 
 # Create your models here.
 class Notes(models.Model):
     topic = models.CharField(max_length=60)
     description = models.TextField(max_length=200)
     upload = models.ImageField(upload_to='notes/', validators=[FileExtensionValidator(allowed_extensions=['jpg', 'jpeg', 'png'])])
+    user = models.ForeignKey(UserAccount, on_delete=models.CASCADE)
     time_posted = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
